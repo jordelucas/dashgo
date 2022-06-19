318e4ecca5f0602681075d3438764d4c033eafe8
Jordevá <jordevalucas@gmail.com>
2022-06-19 10:10:05 -0300

    add signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,39 @@
-export default function Home() {
+import { Button, Flex, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      width='100vw'
+      height='100vh'
+      alignItems='center'
+      justifyContent='center'
+    >
+      <Flex
+        as='form'
+        flexDir='column'
+        width='100%'
+        maxWidth={360}
+        padding={8}
+        borderRadius={8}
+        background='gray.800'
+      >
+        <Stack spacing={4}>
+          <Input name='email' type='email' label='Email'/>
+
+          <Input name='password' type='password' label='Senha'/>
+        </Stack>
+        
+        <Button
+          type='submit'
+          marginTop={6}
+          colorScheme='pink'
+          size='lg'
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
