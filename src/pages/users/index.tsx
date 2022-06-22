f1248b30efc4ae77d60e5f296a87a793d7d97447
Jordevá <jordevalucas@gmail.com>
2022-06-22 15:50:04 -0300

    customize useUsers hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { useQuery } from 'react-query';
 import { RiAddLine } from 'react-icons/ri';
 import {
   Box,
@@ -19,32 +18,14 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 
-import { api } from '../../services/api';
+import { useUsers } from '../../services/hooks/useUsers';
+
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('users');
-     
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 seconds
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
